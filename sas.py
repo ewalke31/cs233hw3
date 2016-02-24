5d068381a86088bbdfade793399c8ceac1f14341
Eric Walker <ewalke31@jhu.edu>
2016-02-23 22:03:21 -0500

    extra line at end

--- sas.py
+++ sas.py
@@ -6,11 +6,11 @@
 import sys
 
 
-# the hex pattern for each SCRAM instruction
+# the binary pattern for each SCRAM instruction
 encoding = {
-    "HLT": 0, "LDA": 1, "LDI": 2,
-    "STA": 3, "STI": 4, "ADD": 5,
-    "SUB": 6, "JMP": 7, "JMZ": 8
+    "HLT": "0000", "LDA": "0001", "LDI": "0010",
+    "STA": "0011", "STI": "0100", "ADD": "0101",
+    "SUB": "0110", "JMP": "0111", "JMZ": "1000"
 }
 
 labels = {}
@@ -44,12 +44,20 @@ def firstPassParser(inputList):
                 if label in labels.keys():
                     print("Label already exists. Error on line: " + inputLine,
                           file=sys.stderr)
+                elif label in encoding.keys():
+                    print("Label cannot be same name as microprogram. Error on line: " +
+                          inputLine, file=sys.stderr)
                 else:
-                    labels.update({label:str(hex(currLine))[2:]})
+                    if currLine < 16: 
+                        labels.update({label:"{:0>4}".format("{0:b}".format(currLine))})
+                    else:
+                        labels.update({label:"{:0>8}".format("{0:b}".format(currLine))})
             currLine += 1
             if currLine >= 16:
-                print("Program is too long for the SCRAM. Error on line: " +
-                      inputLine, file=sys.stderr)
+                for seg in entry:
+                    if seg in encoding.keys() or seg == "DAT":
+                        print("Program is too long for the SCRAM. Error on line: " +
+                              inputLine, file=sys.stderr)
         inputList[inputLine-1] = entry
         inputList[inputLine-1].insert(0, inputLine) #save input line for error checking later
     i = 0
@@ -82,29 +90,29 @@ def secondPassParser(inputList):
     
 def subparser(i, line, output):
     if line[i] in encoding.keys():
-        output += str(encoding[line[i]])
+        output += encoding[line[i]]
         if line[i+1].isdigit():
             if int(line[i+1]) >= 0 and int(line[i+1]) <= 15 and type(line[i+1]) is not float:
-                output += str(hex(int(line[i+1])))[2:]
+                output += "{:0>5}".format("{0:b}".format(int(line[i+1])) + " ")
             else:
                 print("Invalid address format. Error on line: " +
                       line[0], file=sys.stderr)
         else:
             if line[i+1] in labels.keys():
-                output += labels[line[i+1]]
+                output += labels[line[i+1]] + " "
             else:
                 print("Undefined label. Error on line: " + line[0],
                       file=sys.stderr)
     elif line[i] == "DAT":
         if line[i+1].isdigit():
             if int(line[i+1]) >= 0 and int(line[i+1]) <= 255 and type(line[i+1]) is not float:
-                output += str(hex(int(line[i+1])))[2:]
+                output += "{:0>9}".format("{0:b}".format(int(line[i+1])) + " ")
             else:
                 print("Invalid address format. Error on line: " +
                       line[0], file=sys.stderr)
         else:
             if line[i+1] in labels.keys():
-                output += labels[line[i+1]]
+                output += labels[line[i+1]] + " "
             else:
                 print("Undefined label. Error on line: " + line[0],
                       file=sys.stderr)
@@ -118,8 +126,11 @@ def main():
     global labels
     inputList = readInput()
     inputList = firstPassParser(inputList)
-    output = secondPassParser(inputList)
-    print(output, file=sys.stdout)
+    output = secondPassParser(inputList).split()
+    out = ""
+    for byte in output:
+        out += chr(int(byte, 2))
+    print(out, file=sys.stdout)
     
 if __name__ == "__main__":
     main()
